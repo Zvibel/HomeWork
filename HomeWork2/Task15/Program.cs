cb450f3247844e063458fae9c99f3ce6b9e347e6
Vitaly Lukianov <mszvibel@gmail.com>
2023-01-19 15:29:38 +0300

    Добавил решение задачи 15

---
+++ HomeWork2/Task15/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот
+день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.Clear();
+
+Console.WriteLine("Напишите день недели: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if(number == 1){
+Console.WriteLine("Нет");
+}
+else if(number == 2){
+Console.WriteLine("Нет");
+}
+else if(number == 3){
+Console.WriteLine("Нет");
+}
+else if(number == 4){
+Console.WriteLine("Нет");
+}
+else if(number == 5){
+Console.WriteLine("Нет");
+}
+else if(number == 6){
+Console.WriteLine("Да");
+}
+else if(number == 7){
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Такого дня нет");
+}
