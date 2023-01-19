7c8b217a7d20086007e59cfb05628e186102ab30
Vitaly Lukianov <mszvibel@gmail.com>
2023-01-19 15:35:17 +0300

    Правки

--- HomeWork2/Task15/Program.cs
+++ HomeWork2/Task15/Program.cs
@@ -12,28 +12,35 @@ Console.WriteLine("Напишите день недели: ");
 string numberStr = Console.ReadLine();
 int number = Convert.ToInt32(numberStr);
 
-if(number == 1){
-Console.WriteLine("Нет");
+if (number == 1)
+{
+    Console.WriteLine("Нет");
 }
-else if(number == 2){
-Console.WriteLine("Нет");
+else if (number == 2)
+{
+    Console.WriteLine("Нет");
 }
-else if(number == 3){
-Console.WriteLine("Нет");
+else if (number == 3)
+{
+    Console.WriteLine("Нет");
 }
-else if(number == 4){
-Console.WriteLine("Нет");
+else if (number == 4)
+{
+    Console.WriteLine("Нет");
 }
-else if(number == 5){
-Console.WriteLine("Нет");
+else if (number == 5)
+{
+    Console.WriteLine("Нет");
 }
-else if(number == 6){
-Console.WriteLine("Да");
+else if (number == 6)
+{
+    Console.WriteLine("Да");
 }
-else if(number == 7){
-Console.WriteLine("Да");
+else if (number == 7)
+{
+    Console.WriteLine("Да");
 }
 else
 {
-Console.WriteLine("Такого дня нет");
+    Console.WriteLine("Такого дня нет");
 }
