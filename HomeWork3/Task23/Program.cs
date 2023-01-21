fa40db48e4267a4f62d3969ab6d4772144a3410b
Vitaly Lukianov <mszvibel@gmail.com>
2023-01-21 16:10:18 +0300

    Правки

--- HomeWork3/Task23/Program.cs
+++ HomeWork3/Task23/Program.cs
@@ -2,3 +2,12 @@
 
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i*i},");
+}
