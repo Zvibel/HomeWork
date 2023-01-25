fc3abbe5a47dd124e17433005333ede218b935c2
Vitaly Lukianov <mszvibel@gmail.com>
2023-01-25 16:06:57 +0300

    Правки

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -11,17 +11,17 @@ int[] CreateArray()
     Random number = new Random();
     for (int i = 0; i < Array.Length; i++)
     {
-        Array[i] = number.Next(0,100);
+        Array[i] = number.Next(0, 100);
     }
     return Array;
 }
 int[] a = CreateArray();
 
-void PrintArray(int[] array)
+/*void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($" {array[i]} ");
     }
-}
-PrintArray(a);
+}*/
+ Console.WriteLine("[{0}]", string.Join(", ", a));
