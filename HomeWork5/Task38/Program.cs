5b2a3e47c4c33377cc2e899814f5147adcc6ce5d
Vitaly Lukianov <mszvibel@gmail.com>
2023-01-30 17:27:51 +0300

    Добавил решение задачи №38

---
+++ HomeWork5/Task38/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным 
+элементов массива.
+[3 7 22 2 78] -> 76*/
+
+
+int[] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+int[] array = InitArray(5);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine($"-> разница между максимальным и минимальным элементами массива = {max - min}.");
+
+
