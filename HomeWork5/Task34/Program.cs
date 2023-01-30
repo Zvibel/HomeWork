723327e0e38fffe51a54aced3bef61f196a3bb46
Vitaly Lukianov <mszvibel@gmail.com>
2023-01-30 15:30:10 +0300

    Добавил решение задачи №34

---
+++ HomeWork5/Task34/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу,
+ которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] InitArray(int dimension)
+{
+  int[] arr = new int[dimension];
+  Random rnd = new Random();
+
+  for (int i = 0; i < dimension; i++)
+  {
+    arr[i] = rnd.Next(100, 1000);
+  }
+  return arr;
+}
+
+int FindCount(int[] arr)
+{
+  var count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] % 2 == 0)
+      count++;
+  }
+  return count;
+}
+
+Console.WriteLine();
+
+int[] array = InitArray(10);
+int count = FindCount(array);
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+Console.WriteLine($"-> В указанном массиве находится {count} четных числа чисел.");
