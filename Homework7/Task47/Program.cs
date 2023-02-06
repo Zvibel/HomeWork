84f8792108333ced7b10e944df6e2c57edecca30
Vitaly Lukianov <mszvibel@gmail.com>
2023-02-06 14:49:14 +0300

    Добавил решение задачи №47

--- Homework7/Task47/Program.cs
+++ Homework7/Task47/Program.cs
@@ -5,3 +5,41 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9*/
 
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] InitMatrix(int rows, int columns)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = Convert.ToDouble(rnd.Next(-100, 100) / 10.0);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = InitMatrix(rows, columns);
+
+PrintMatrix(matrix);
